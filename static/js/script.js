a90f10b087c360ad9dda1dfd14189d8b3faa4779
Eiko <gaia.e.h@gmail.com>
2020-06-23 01:39:14 +0800

    在写了在写了.jpg

--- static/js/script.js
+++ static/js/script.js
@@ -1,126 +1,84 @@
-"use strict";
-console.log('script.js 已载入');
-let 临时输出;
-$(document).ready(function () {
-    let
-        载入次数 = 0,
-        载入的JSON = [],
-        歌单 = [],
-        选项 = {},
-        难度文字 = {
-            'easy': '梅',
-            'normal': '竹',
-            'hard': '松',
-            'oni': '鬼'
-        },
-        歌单名字 = {
-            'taiko14old': '旧框14旧基准',
-            'taiko14new': '旧框14【新】基准',
-            'taikomomoiroSP': '摸摸衣裸SP',
-            'taikoRedSP': '红SP'
-        };
-    function 载入歌单(文件名, 版本 = 1) {
-        $.ajax({
-            type: "GET",
-            url: "./data/" + 文件名 + ".json?ver=" + 版本,
-            dataType: "json",
-        }).done(
-            function (响应) {
-                console.log(响应);
-                载入的JSON[文件名] = 响应;
-                console.log(载入的JSON);
-            }
-        ).fail(
-            // 万一网不好或者被劫持就重新加载一下
-            function () {
-                if (载入次数 < 10) {
-                    载入次数++;
-                    载入歌单(文件名, 版本);
-                    console.log('载入文件失败：' + 文件名 + '（' + 版本 + '），失败次数：' + 载入次数);
-                } else {
-                    alert(文件名 + '.json' + '（' + 版本 + '）' + '载入失败次数过多，请刷新页面重试');
-                    载入次数 = 0;
-                }
-            }
-        );
-    };
-    载入歌单('taiko14old', 1);
-    载入歌单('taiko14new', 1);
-    载入歌单('taikomomoiroSP', 20191130);
-    载入歌单('taikoRedSP', 2020010901);
-    // 载入歌单('error', 123);
-
-    //#region 设置相关
-    // 点击展示设置框
-    $(document).on('click', '#option-button', function () {
-        $('.option-main').addClass('show');
-    });
-    // 设置框点击提交动作
-    $(document).on('submit', '#option-form', function () {
-        // 获取表单数据
-        let 表单数据 = $(this).serializeArray();
-        console.log('表单数据：', 表单数据);
-        表单数据.forEach(function (项目) {
-            选项[项目.name] = 项目.value;
-        })
-        console.log('选项：', 选项);
-
-        // 修改显示内容
-        $('#option-show-difficulty').text(难度文字[选项.抽取难度]);
-        $('#option-show-level-num-min').text(选项.最低等级);
-        $('#option-show-level-num-max').text(选项.最高等级);
-        $('#option-show-songlist').text(歌单名字[选项.已选歌单]);
-
-        // 处理歌单
-        歌单 = 载入的JSON[选项.已选歌单].songList;
-        console.log('筛选前歌单：', 歌单);
-        function 筛选歌曲(歌曲) {
-            return 歌曲.level[选项.抽取难度] >= 选项.最低等级 && 歌曲.level[选项.抽取难度] <= 选项.最高等级;
-        }
-        歌单 = 歌单.filter(筛选歌曲);
-        console.log('筛选后歌单：', 歌单);
-
-        // 关闭设置框
-        $('.option-main').removeClass('show');
-        $('#roll-button').prop('disabled', false);
-        return false;
-    });
-    //#endregion 设置相关
-
-    //#region 抽奖相关
-    function 抽取(最小值, 最大值) {
-        let 抽取数 = 最大值 - 最小值 + 1;
-        return Math.floor(Math.random() * 抽取数 + 最小值);
+'use strict';
+/**
+ * Doc Ready
+ * 就是 $.(document).ready(function(){})
+ */
+function docReady(fn) {
+    if (document.readyState != 'loading') {
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
     }
-    // 抽！
-    $(document).on('click', '#roll-button', function () {
-        // 先禁了防止鼠标双击或者手抖
-        $('#roll-button').prop('disabled', true);
-        let A = 0;
-        function 抽取歌曲() {
-            let 随机数, 抽到的歌;
-            随机数 = 抽取(0, 歌单.length - 1);
-            抽到的歌 = 歌单[随机数];
-            console.log(抽到的歌);
+}
 
-            // 显示歌曲内容
-            $('#result-name').text(抽到的歌.name);
-            $('#result-name-cn').text(抽到的歌.name_cn);
-            $('#result-category').text(抽到的歌.category);
-            $('#result-level-name').text(难度文字[选项.抽取难度]);
-            $('#result-level-num').text(抽到的歌.level[选项.抽取难度]);
+/**
+ * Get Element
+ * 就是 $(element)
+ */
+function getEl(element) {
+    let tempEl;
+    switch ([...element][0]) {
+        case '#':
+            tempEl = document.querySelectorAll(element)[0];
+            break;
+        default:
+            tempEl = document.querySelectorAll(element);
+    };
+    return tempEl;
+}
 
-            // 抽完之后把按钮还原
-            A++;
-            console.log('A:', A);
-            if (A > 50) {
-                $('#roll-button').prop('disabled', false);
-                A = 0;
-            }
+/**
+ * My ajax
+ * 自写的一个稀烂的 Ajax 函数
+ */
+function myAjax({
+    method = 'GET',
+    url = undefined,
+    data,
+    async = true,
+    user = null,
+    password = null,
+    type = '',
+    done = () => { },
+    fail = () => { },
+    always = () => { },
+    error = () => { },
+    timeout = undefined,
+    timeoutEvent = () => { },
+    beforeSend = () => { },
+}) {
+
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url, async, user, password);
+    xhr.responseType = type;
+    beforeSend(this);
+    xhr.onload = function () {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            done(this);
+        } else {
+            fail(this);
         }
-        for (let a = 0; a <= 50; a++) {
-            setTimeout(抽取歌曲, 50 * a);
+        always(this);
+    }
+    xhr.onerror = function () {
+        error(this);
+        always(this);
+    }
+    timeout = Number(timeout);
+    if (!Number.isNaN(timeout) && Number.isInteger(timeout)) {
+        xhr.timeout = timeout;
+        xhr.ontimeout = function () {
+            timeoutEvent(this);
+            xhr.abort();
+            always(this);
         }
-    });
-    //#endregion 抽奖相关
-});
+    }
+    if (data) {
+        xhr.send(data);
+    } else {
+        xhr.send();
+    }
+};
+
+// 下面才是正文
+console.log('script.js 已载入');
