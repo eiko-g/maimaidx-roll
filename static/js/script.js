8dc4b5ea2b2bcfc6b5124aad5049069f2608e03e
Eiko <gaia.e.h@gmail.com>
2020-06-24 02:54:25 +0800

    在写了在写了.png

--- static/js/script.js
+++ static/js/script.js
@@ -79,6 +79,51 @@ function myAjax({
         xhr.send();
     }
 };
-
+window.设置 = {};
 // 下面才是正文
 console.log('script.js 已载入');
+docReady(() => {
+    let
+        Roll歌按钮 = getEl('#roll-button');
+    Roll歌按钮.setAttribute('disabled', 'disabled');
+    //#region 设置
+    let 设置按钮 = getEl('#setting-button');
+    let 保存设置 = getEl('#save-option');
+    设置按钮.addEventListener('click', () => {
+        getEl('.option-main')[0].classList.add('show');
+    });
+    let 表单 = getEl('#option-form');
+    表单.addEventListener('submit', function (event) {
+        let
+            最低等级 = getEl('#lv-min').value,
+            最高等级 = getEl('#lv-max').value,
+            最低等级带加号 = getEl('#lv-min-plus').checked,
+            最高等级带加号 = getEl('#lv-max-plus').checked,
+            表单数据 = new FormData(this);
+        console.log('表单数据：', 表单数据);
+
+        if (!(Number(最低等级) > 0) || isNaN(Number(最低等级)) || !(Number(最高等级) > 0) || isNaN(Number(最高等级))) {
+            console.log('设置有误');
+            console.log('最低等级：', 最低等级);
+            console.log('最高等级：', 最高等级);
+
+            console.log(Number(最低等级));
+            console.log(isNaN(Number(最低等级)));
+            console.log(Number(最高等级));
+            console.log(isNaN(Number(最高等级)));
+
+            // event.preventDefault();
+            return false;
+        }
+        设置 = 表单数据;
+        getEl('.option-main')[0].classList.remove('show');
+        Roll歌按钮.removeAttribute('disabled');
+        event.preventDefault();
+        return false;
+    });
+    //#endregion
+
+    Roll歌按钮.addEventListener('click', () => {
+        console.log('Roll!');
+    });
+});
